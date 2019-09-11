f005977365718d0ad0974740e92baa52dbd5330d
amullabaev <a.mullabaev@gmail.com>
2019-09-12 01:09:37 +0600

    added configuration properties handling

--- src/main/kotlin/com/example/market/MarketApplication.kt
+++ src/main/kotlin/com/example/market/MarketApplication.kt
@@ -1,9 +1,12 @@
 package com.example.market
 
+import com.example.market.properties.CustomProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(CustomProperties::class)
 class MarketApplication
 
 fun main(args: Array<String>) {
