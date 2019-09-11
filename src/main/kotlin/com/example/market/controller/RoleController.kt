f12f85ee73c2c7b05d289c62b745690816011db5
amullabaev <a.mullabaev@gmail.com>
2019-09-12 01:38:21 +0600

    Refactored controllers, added services in controllers

--- src/main/kotlin/com/example/market/controller/RoleController.kt
+++ src/main/kotlin/com/example/market/controller/RoleController.kt
@@ -1,16 +1,16 @@
 package com.example.market.controller
 
 import com.example.market.entity.Role
-import com.example.market.repository.RoleRepository
+import com.example.market.service.RoleService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/roles")
-class RoleController(private val roleRepository: RoleRepository) {
+class RoleController(private val roleService: RoleService) {
     @GetMapping
     fun findAll(): Iterable<Role> {
-        return roleRepository.findAll()
+        return roleService.findAll()
     }
 }
--- src/main/kotlin/com/example/market/controller/UserController.kt
+++ src/main/kotlin/com/example/market/controller/UserController.kt
@@ -1,38 +1,28 @@
 package com.example.market.controller
 
 import com.example.market.dto.UserDto
-import com.example.market.entity.Role
 import com.example.market.entity.User
-import com.example.market.repository.RoleRepository
-import com.example.market.repository.UserRepository
+import com.example.market.service.UserService
 import org.springframework.web.bind.annotation.*
 import java.util.*
 
 @RestController
 @RequestMapping("/api/users")
-class UserController(private val userRepository: UserRepository, private val roleRepository: RoleRepository) {
+class UserController(private val userService: UserService) {
 
     @GetMapping
     fun findAll(): Iterable<User> {
-        return userRepository.findAll()
+        return userService.findAll()
     }
 
     @GetMapping("/{id}")
     fun findById(@PathVariable id: Long): Optional<User> {
-        return userRepository.findById(id)
+        return userService.findById(id)
     }
 
     @PostMapping("/create")
     fun create(@RequestBody userDto: UserDto): User {
-        val role: Role = roleRepository.getOne(userDto.roleId)
-        val user = User(
-                firstName = userDto.firstName,
-                lastName = userDto.lastName,
-                email = userDto.email,
-                role = role,
-                rowCreatedDate = Date()
-        )
-        return userRepository.save(user)
+        return userService.create(userDto)
     }
 
 }
