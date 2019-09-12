69d38e17d625afb571798f177683522033e8cf1f
amullabaev <a.mullabaev@gmail.com>
2019-09-12 11:10:30 +0600

    Added Category and Product dtos

---
+++ src/main/kotlin/com/example/market/dto/ProductDto.kt
@@ -0,0 +1,9 @@
+package com.example.market.dto
+
+class ProductDto(
+        var name: String,
+        var description: String,
+        var amount: Long,
+        var price: Long,
+        var categoryId: Long
+)
