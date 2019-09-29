134f51dabe9f4b9d058da640b633cde9d43f8af4
amullabaev <a.mullabaev@gmail.com>
2019-09-29 19:30:49 +0600

    Changed Product and Category service interfaces

--- src/main/kotlin/com/example/market/service/CategoryService.kt
+++ src/main/kotlin/com/example/market/service/CategoryService.kt
@@ -2,10 +2,11 @@ package com.example.market.service
 
 import com.example.market.dto.CategoryDto
 import com.example.market.entity.Category
-import java.util.*
 
 interface CategoryService {
     fun findAll(): Iterable<Category>
-    fun findById(id: Long): Optional<Category>
+    fun findById(id: Long): Category?
     fun create(categoryDto: CategoryDto): Category
+    fun update(id: Long, categoryDto: CategoryDto): Category
+    fun destroy(id: Long)
 }
--- src/main/kotlin/com/example/market/service/ProductService.kt
+++ src/main/kotlin/com/example/market/service/ProductService.kt
@@ -2,10 +2,11 @@ package com.example.market.service
 
 import com.example.market.dto.ProductDto
 import com.example.market.entity.Product
-import java.util.*
 
 interface ProductService {
     fun findAll(): Iterable<Product>
-    fun findById(id: Long): Optional<Product>
+    fun findById(id: Long): Product?
     fun create(productDto: ProductDto): Product
+    fun update(id: Long, productDto: ProductDto): Product
+    fun destroy(id: Long)
 }
